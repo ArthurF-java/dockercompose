6b55c3668833dd3e0ccd5715d9d837d23cf2156c
Arthur <fonichkin.arthur@gmail.com>
2025-01-12 08:11:34 +0300

    init

---
+++ src/test/java/com/dfd/dockercompose/DockercomposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfd.dockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockercomposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
